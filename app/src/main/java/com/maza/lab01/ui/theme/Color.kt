91713702639d13c4e4a45a1549e809790aac7c1d
DarioMaza <dario.maza@tecsup.edu.pe>
2026-08-21 15:11:35 -0500

    Se modificaron los colores de la app correctamente

--- app/src/main/java/com/maza/lab01/ui/theme/Color.kt
+++ app/src/main/java/com/maza/lab01/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.maza.lab01.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Purple80 = Color(0xFF90CAF9)
+val PurpleGrey80 = Color(0xFFB0BEC5)
+val Pink80 = Color(0xFF81D4FA)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Purple40 = Color(0xFF1976D2)
+val PurpleGrey40 = Color(0xFF455A64)
+val Pink40 = Color(0xFF0288D1)
